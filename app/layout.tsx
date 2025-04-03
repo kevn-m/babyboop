ed6bdce4016f3baaf77cff663e8306c3884c9e4d
Kevin Mok <south.kay@gmail.com>
2025-04-03 20:08:55 +1100

    Strip some boilerplate out

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Babyboop",
+  description: "Babyboop",
 }
 
 const geistSans = Geist({
@@ -40,14 +40,8 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
+                  <HeaderAuth />
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { type NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  return
 }
 
 export const config = {
@@ -17,4 +16,4 @@ export const config = {
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-};
+}
